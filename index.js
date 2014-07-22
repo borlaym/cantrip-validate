1e11d505f2ecf3bc5f7383a5613439c68b43ed8f
Márton Borlay <marton.borlay@kriek.hu>
2014-07-22 12:48:09 +0200

    On the road to being compliant with the json schema specification

--- index.js
+++ index.js
@@ -1,158 +1,46 @@
 var _ = require("lodash");
 var fs = require("fs");
-
-var validations = {};
-
-//Load all installed validations from node_modules. Directory names should start with "cantrip-validate-"
-var dirs = fs.readdirSync("./node_modules");
-for (var i = 0; i < dirs.length; i++) {
-	if(fs.statSync("./node_modules/" + dirs[i]).isDirectory()) {
-		if (dirs[i].indexOf("cantrip-validate-") === 0) {
-			validations[dirs[i].replace("cantrip-validate-", "")] = require(dirs[i]);
-		}
-	}
-}
+var validate = require('jsonschema').validate;
 
 var Validation = {
 	/**
-	 * The wrapper method for validation. Checks if we have validation in our JSON.
-	 * If we do, it runs the data through it, returning either true or false based on whether the data passed the test or not.
-	 * If the JSON doesn't support validation, it automatically returns true.
+	 * Validate the whole request object. Gets the validation schema from the _schema meta object, if one exists.
+	 * Then validates the request data, and return an error to the next() method if it's invalid, null otherwise
 	 * @param  {Object} request From Express
 	 * @param  {Object} response From Express
 	 * @return {boolean}
 	 */
 	validate: function(req, res, next) {
-		var validation = Validation.getValidation("root", req);
-		console.log(validation);
-		if (validation) {
-			next(Validation.validateObject(req.body, validation, req));
-		} else {
-			next();			
-		}
-	},
-	/**
-	 * Gets a validation object from the metadata.
-	 * @param  {String} typeName The name of the validation object. The ke "root" is special and signifies the whole JSON object
-	 * @param  {Object} req      The Request object
-	 * @return {Object}          Returns a validation object. If it doesn't exists, returns false.
-	 */
-	getValidation: function(typeName, req) {
-		var schema = req.data._schema;
-		if (schema) {
-			//When we check the root, we go through the schema following the path.
-			//When we use this function later to dig into another type in the schema, we just return that object
-			if (typeName !== "root") {
-				return schema[typeName];
-			}
-			var validation = schema["root"];
-			//go through the schema following the path
-			for (var i = 0; i < req.pathMembers.length; i++) {
-				if (validation[req.pathMembers[i]] !== undefined) {
-					validation = validation[req.pathMembers[i]];
-					//if it's an object, get out of the current type and search for that type on the schema root
-					if (validation.type === "object") {
-						validation = schema[validation.schema];
-					} else if (validation.type === "collection") {
-						validation = schema[validation.schema];
-					}
-				} else {
-					//We are in a model inside a collection
-					validation = validation;
-				}
-			}
-			return validation;
-		} else {
-			return false;
-		}
-	},
-	validateObject: function(object, validation, req) {
-		//If object is not an object return error
-		if (!_.isObject(object)) {
-			return {
-				status: 400,
-				error: "Type error. Expected object, found " + object + "."
-			};
-		}
-		//If the posted object doesn't contain all keys needed for the validation, throw an error
-		//But only if the object is not equal to the request body, because when we PUT the object, we don't want to specify all keys
-		for (var key in validation) {
-			if (object[key] === undefined && object !== req.body) {
-				return {
-					status: 400,
-					error: "Type error. Missing key "+key+"."
-				};
-			}
-		}
-		//Loop through the keys in the posted object
-		for (var key in object) {
-			var v = validation[key];
-			//return false if we try to validate a key that doesn't exist in the schema
-			if (v === undefined) {
-				return {
-					status: 400,
-					error: "Type error. Invalid key "+key+" in object."
-				};
-			}
-			//Check type
-			if (!Validation.checkType(object[key], v, req)) {
-				var correctType = v.type === "object" || v.type === "collection" ? v.schema : v.type;
-				return {
-					status: 400,
-					error: "Type error. Key " + key + " must be of type " + correctType
-				};
-			}
-			
-			//Now run all custom validations defined in an array in the key "validation"
-			if (v.validation) {
-				var customValidation = validations[v.type].validate(object[key], v.validation);
-				if (!customValidation.valid) {
-					return {
-						status: 400,
-						error: "Validation error with key "+key+". " + customValidation.message
-					};
-				}
-			}
-		}
-	},
-	checkType: function(value, validation, req) {
-		var type = validation.type;
-		if (type === "boolean") {
-			if (_.isBoolean(value)) return true;
-			else return false;
-		} else if (type === "string") {
-			if (_.isString(value)) return true;
-			else return false;
-		} else if (type === "number") {
-			if (_.isNumber(value)) return true;
-			else return false;
-		} else if (type === "object") {
-			//If we specified a schema, then the object must be of a given shema. If we didn't, its free for all!
-			if (validation.schema === undefined && _.isObject(value)) return true;
-			if (!this.validateObject(value, this.getValidation(validation.schema, req), req)) return true;
-			else return false;
-		} else if (type === "collection") {
-			if (validation.schema === undefined && _.isArray(value)) return true;
+		if (req.method === "GET") return next();
+		Validation.getValidation(req, function(schema) {
+			var v = validate(req.body, schema);
+			if (v.errors.length === 0) next();
 			else {
-				if (_.indexOf(["string", "number", "boolean"], validation.schema) > -1) {
-					var valid = true;
-					for (var i = 0; i < value.length; i++) {
-						if (!Validation.checkType(value[i], {
-							type: validation.schema
-						}, req)) valid = false;
-					}
-					return valid;
-				} else {
-					var valid = true;
-					for (var i = 0; i < value.length; i++) {
-						if (!_.isObject(value[i])) valid = false;
-						if (this.validateObject(value[i], this.getValidation(validation.schema, req), req)) valid = false;
-					}
-					return valid;
-				}
+				next(v.errors);
 			}
-		}
+		});
+	},
+
+	getValidation: function(req, callback) {
+		//Get the whole schema for the application
+		req.dataStore.get("/_schema", function(err, schema) {
+			//If we didnt find anything, return with null
+			if (!schema) {
+				callback(null);
+				return;
+			}
+			//else we need to loop through it and find the fragment needed for this request
+			var path = (req.path + "").replace("/_contents", "").split("/");
+			if (_.last(path) === "") path.pop(); 
+			path.splice(0,1);
+			while (path.length > 0) {
+				var node = path.pop();
+				schema = schema.properties[node];
+			}
+
+			callback(schema);
+
+		});
 	}
 }
-
 module.exports = Validation.validate;
--- server.js
+++ server.js
@@ -2,5 +2,15 @@ var cantrip = require("Cantrip");
 var validation = require("./index.js");
 
 cantrip.use(validation);
+cantrip.use(function(err, req, res, next) {
+	if( Object.prototype.toString.call( err ) === '[object Array]' ) {
+		res.status(400).send({
+			error: "Validation error",
+			stack: err
+		});
+	} else {
+		next(err);
+	}
+});
 
 cantrip.start();
